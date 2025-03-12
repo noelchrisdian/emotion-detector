e8a7b8e4cef728965d71c137542ed5844e896462
noelchrisdian <noelchrisdian@gmail.com>
2025-03-12 13:30:48 -0400

    Updated emotion_detection.py and created __init__.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotionDetector(text):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,15 @@ def emotionDetector(text):
     payload = {'raw_document': {'text': text}}
     
     response = requests.post(url, headers=headers, json=payload)
-    return response.text
+    response = json.loads(response.text)
+    emotions = response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    output = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion,
+    }
+    return output
